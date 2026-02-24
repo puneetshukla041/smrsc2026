b6c766d7f8e342f730c003b2eb52b3dc24b13e8d
Puneet Shukla <puneetshukla041@gmail.com>
2026-02-24 09:55:55 +0530

    Update footer.jsx

--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -134,7 +134,7 @@ const Footer = () => {
                   <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231 5.45-6.231h0.001zm-1.161 17.52h1.833L7.084 4.126H5.117l11.966 15.644z" />
                 </svg>
             </a>
-            <a href="#" className="text-white hover:text-[#E3F5F6] transition-colors"><Instagram size={20} /></a>
+            <a href="https://teams.microsoft.com/l/message/19:11530ac0-0153-455f-bf9a-274d1cc1995d_1f599023-a430-499c-9056-404cf5107e88@unq.gbl.spaces/1771906141203?context=%7B%22contextType%22%3A%22chat%22%7D" className="text-white hover:text-[#E3F5F6] transition-colors"><Instagram size={20} /></a>
             <a href="#" className="text-white hover:text-[#E3F5F6] transition-colors"><Linkedin size={20} /></a>
             <a href="mailto:smrsc@ssinnovations.org" className="text-white hover:text-[#E3F5F6] transition-colors"><Mail size={20} /></a>
             <a href="tel:+918130027337" className="text-white hover:text-[#E3F5F6] transition-colors"><Phone size={20} /></a>
