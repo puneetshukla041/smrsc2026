53c6ee9f25caffffbf599881071bfd9faa5d501e
Puneet Shukla <puneetshukla041@gmail.com>
2026-02-26 10:17:42 +0530

    Update page.jsx

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -375,7 +375,7 @@ const RegisterPage = () => {
                           )}
                         </div>
                       </div>
-                      <span className="text-[#1A2E35] font-medium text-[15px]">Register as International Delegates Registration (USD)</span>
+                      <span className="text-[#1A2E35] font-medium text-[15px]">Register as International Delegates Registration</span>
                     </label>
 
                   </div>
