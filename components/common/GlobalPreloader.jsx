56499af8b6d3f5450b8cf3c06424a37a7aec2fe7
Puneet Shukla <puneetshukla041@gmail.com>
2026-02-26 16:52:55 +0530

    Update GlobalPreloader.jsx

--- components/common/GlobalPreloader.jsx
+++ components/common/GlobalPreloader.jsx
@@ -1,18 +1,26 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-const ASSETS_TO_PRELOAD = [
+// TOP PRIORITY: These must load before everything else
+const HERO_PRIORITY = [
+  "/images/home/section1/image2.webp",
+  "/images/home/section1/image3.webp",
+  "/images/home/section1/image4.webp",
+  "/images/home/section1/image5.webp",
+  "/images/home/section1/image6.webp",
+  "/images/home/section1/mobileimg.png",
+];
+
+const OTHER_ASSETS = [
   // --- FONTS ---
   "/fonts/BlauerNue.otf",
   "/fonts/Roba-Regular.ttf",
-
   // --- ABOUT : BENEFITS ---
   "/images/about/benifit/image1.webp",
   "/images/about/benifit/image2.webp",
   "/images/about/benifit/image3.webp",
   "/images/about/benifit/image4.webp",
-
   // --- ABOUT : CARDIAC ---
   "/images/about/cardiac/per1.webp",
   "/images/about/cardiac/per2.webp",
@@ -39,7 +47,6 @@ const ASSETS_TO_PRELOAD = [
   "/images/about/cardiac/per23.webp",
   "/images/about/cardiac/per24.webp",
   "/images/about/cardiac/per25.webp",
-
   // --- ABOUT : COMMITTEE ---
   "/images/about/committe/1.webp",
   "/images/about/committe/2.webp",
@@ -81,22 +88,18 @@ const ASSETS_TO_PRELOAD = [
   "/images/about/committe/38.webp",
   "/images/about/committe/39.webp",
   "/images/about/committe/40.webp",
-
   // --- ABOUT : FACULTY ---
   "/images/about/faculty/image1.png",
   "/images/about/faculty/image1.webp",
-
   // --- ABOUT : GENERAL ---
   "/images/about/general/per1.webp",
   "/images/about/general/per2.webp",
   "/images/about/general/per3.webp",
   "/images/about/general/per4.webp",
-
   // --- ABOUT : SECTION 1 ---
   "/images/about/section1/image1.webp",
   "/images/about/section1/img1.webp",
   "/images/about/section1/mobile.webp",
-
   // --- ABOUT : SECTION 2 ---
   "/images/about/section2/image1.webp",
   "/images/about/section2/image2.webp",
@@ -105,7 +108,6 @@ const ASSETS_TO_PRELOAD = [
   "/images/about/section2/image5.webp",
   "/images/about/section2/image6.webp",
   "/images/about/section2/image7.webp",
-
   // --- ABOUT : UROLOGY ---
   "/images/about/urology/clippath.webp",
   "/images/about/urology/image2.webp",
@@ -143,7 +145,6 @@ const ASSETS_TO_PRELOAD = [
   "/images/about/urology/per32.webp",
   "/images/about/urology/per33.webp",
   "/images/about/urology/per34.webp",
-
   // --- EXPLORE ---
   "/images/explore/hero.webp",
   "/images/explore/image1.webp",
@@ -155,22 +156,12 @@ const ASSETS_TO_PRELOAD = [
   "/images/explore/image7.webp",
   "/images/explore/image8.webp",
   "/images/explore/mobile.png",
-
-  // --- HOME : SECTION 1 ---
-  "/images/home/section1/image2.webp",
-  "/images/home/section1/image3.webp",
-  "/images/home/section1/image4.webp",
-  "/images/home/section1/image5.webp",
-  "/images/home/section1/image6.webp",
-  "/images/home/section1/mobileimg.png",
-
   // --- HOME : SECTION 2 ---
   "/images/home/section2/card.webp",
   "/images/home/section2/image1.webp",
   "/images/home/section2/image2.webp",
   "/images/home/section2/image3.webp",
   "/images/home/section2/image4.webp",
-
   // --- HOME : SECTION 3 ---
   "/images/home/section3/image1.webp",
   "/images/home/section3/image2.webp",
@@ -184,13 +175,11 @@ const ASSETS_TO_PRELOAD = [
   "/images/home/section3/mob3.webp",
   "/images/home/section3/mob4.webp",
   "/images/home/section3/mob5.webp",
-
   // --- HOME : SECTION 4 ---
   "/images/home/section4/image1.webp",
   "/images/home/section4/image2.webp",
   "/images/home/section4/image3.webp",
   "/images/home/section4/image4.webp",
-
   // --- HOME : SECTION 5 ---
   "/images/home/section5/image1.webp",
   "/images/home/section5/image2.webp",
@@ -200,12 +189,10 @@ const ASSETS_TO_PRELOAD = [
   "/images/home/section5/mob2.webp",
   "/images/home/section5/mob3.webp",
   "/images/home/section5/mob4.webp",
-
   // --- HOME : SECTION 6 ---
   "/images/home/section6/image1.webp",
   "/images/home/section6/mobile.webp",
   "/images/home/section6/image1.png",
-
   // --- MEDIA ---
   "/images/media/blog1.png",
   "/images/media/blog2.png",
@@ -220,94 +207,35 @@ const ASSETS_TO_PRELOAD = [
   "/images/media/mobile.webp",
   "/images/media/press1.webp",
   "/images/media/press2.webp",
-
-  // --- PAST EVENT : 2025 ---
+  // --- PAST EVENT ---
   "/images/pastevent/2025/image1.webp",
-  "/images/pastevent/2025/image2.webp",
-  "/images/pastevent/2025/image3.webp",
-  "/images/pastevent/2025/image4.webp",
-  "/images/pastevent/2025/image5.webp",
-  "/images/pastevent/2025/image6.webp",
-  "/images/pastevent/2025/image7.webp",
-  "/images/pastevent/2025/image8.webp",
-  "/images/pastevent/2025/image9.webp",
-  "/images/pastevent/2025/image10.webp",
-  "/images/pastevent/2025/image11.webp",
-  "/images/pastevent/2025/image12.webp",
-
-  // --- PAST EVENT : 24 ---
   "/images/pastevent/24/image1.webp",
-  "/images/pastevent/24/image2.webp",
-  "/images/pastevent/24/image3.webp",
-  "/images/pastevent/24/image4.webp",
-  "/images/pastevent/24/image5.webp",
-  "/images/pastevent/24/image6.webp",
-  "/images/pastevent/24/image7.webp",
-  "/images/pastevent/24/image8.webp",
-  "/images/pastevent/24/image9.webp",
-  "/images/pastevent/24/image10.webp",
-  "/images/pastevent/24/image11.webp",
-  "/images/pastevent/24/image12.webp",
-
   // --- VISIT ---
   "/images/visit/airport.webp",
   "/images/visit/hero.webp",
   "/images/visit/image.webp",
   "/images/visit/mobile.png",
-  "/images/visit/place1.webp",
-  "/images/visit/place2.webp",
-  "/images/visit/place3.webp",
-  "/images/visit/place4.webp",
-  "/images/visit/place5.webp",
-  "/images/visit/place6.webp",
-  "/images/visit/place7.webp",
-  "/images/visit/place8.webp",
-  "/images/visit/place9.webp",
-  "/images/visit/veneu.webp",
-  "/images/visit/venue1.webp",
-  "/images/visit/venue2.webp",
-  "/images/visit/venue3.webp",
-  "/images/visit/venue4.webp",
-  "/images/visit/venue5.webp",
-  "/images/visit/venue6.webp",
-
-  // --- LOGOS ---
   "/logos/header.png",
   "/logos/ssilogo.png",
-  
-  // --- LOGOS : ANIMATION ---
   "/logos/animation/1.png",
-  "/logos/animation/2.png",
-  "/logos/animation/3.png",
-  "/logos/animation/4.png",
-  "/logos/animation/5.png",
-  "/logos/animation/6.png",
-  "/logos/animation/7.png",
-  "/logos/animation/8.png",
-  "/logos/animation/9.png",
-  "/logos/animation/10.png",
-  "/logos/animation/11.png",
   "/logos/animation/12.png",
-  "/logos/animation/Isolation_Mode (1).png",
 ];
 
-const VIDEOS_TO_PRELOAD = [
-  "/videos/Color.webm",
-  "/videos/smrsc24.webm",
-];
+const VIDEOS = ["/videos/Color.webm", "/videos/smrsc24.webm"];
 
 export default function GlobalPreloader() {
   useEffect(() => {
+    const fetchAsset = (src) =>
+      fetch(src, { cache: "force-cache", mode: "no-cors" }).catch(() => {});
+
     const preloadAll = async () => {
-      const allMedia = [...ASSETS_TO_PRELOAD, ...VIDEOS_TO_PRELOAD];
-      
-      // Fire everything at once
-      await Promise.all(
-        allMedia.map(src => 
-          fetch(src, { cache: 'force-cache', mode: 'no-cors' })
-            .catch(() => {/* fail silently */})
-        )
-      );
+      // 1. Load Hero Priority assets FIRST and wait for them
+      await Promise.all(HERO_PRIORITY.map(fetchAsset));
+      console.log("Priority 1: Hero images downloaded.");
+
+      // 2. Load everything else in the background now that the hero is safe
+      const remainingAssets = [...OTHER_ASSETS, ...VIDEOS];
+      Promise.all(remainingAssets.map(fetchAsset));
     };
 
     preloadAll();
