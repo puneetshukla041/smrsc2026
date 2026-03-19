7bd2044a8b038f3066f52916d8bcc1d859d2eff7
Puneet Shukla <226358522+puneetshukla041@users.noreply.github.com>
2026-03-19 16:45:33 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* --- GOOGLE ANALYTICS (UPDATED WITH ID FROM IMAGE) --- */}
+        {/* --- GOOGLE ANALYTICS --- */}
         <Script 
           strategy="afterInteractive" 
           src="https://www.googletagmanager.com/gtag/js?id=G-E4SWQKYNZ6" 
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
           }}
         />
 
-        {/* --- GOOGLE TAG MANAGER (Kept from your earlier message) --- */}
+        {/* --- GOOGLE TAG MANAGER --- */}
         <Script
           id="gtm-script"
           strategy="afterInteractive"
@@ -103,6 +103,37 @@ export default function RootLayout({ children }) {
         <div className="md:hidden">
           <Timer />
         </div>
+
+        {/* --- LINKEDIN INSIGHT TAG --- */}
+        <Script
+          id="linkedin-insight"
+          strategy="lazyOnload" // Loads after the page finishes rendering so it doesn't slow down your site
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "8948196";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+              (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+            `,
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }} 
+            alt="" 
+            src="https://px.ads.linkedin.com/collect/?pid=8948196&fmt=gif" 
+          />
+        </noscript>
+        
       </body>
     </html>
   );
